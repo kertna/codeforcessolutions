9b2546676b37063342dffaa5f3d61127ab18bab3
keerthana <44168551+kertna@users.noreply.github.com>
2020-10-16 11:56:10 +0530

    Create CurrencySysteminGeraldion.cpp

---
+++ CurrencySysteminGeraldion.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+ 
+    
+        
+      
+      int n;
+      int x,i;
+      cin>>n;
+      for(i=0;i<n;i++)
+        {
+            cin>>x;
+            if(x==1)
+            {
+                cout<<"-1";
+                break;
+            }
+        }
+        if(i==n)
+        {
+            cout<<"1";
+        }
+    
+    
+                
+    return 0;
+}
